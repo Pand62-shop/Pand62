26f8f9762ccf2d9c64bf263223b67b63513e985b
MadstonMagic <anouk.gemmeke@ziggo.nl>
2021-09-03 19:38:29 +0200

    Mobile Menu

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
 	$('li.about-menu a').on('click', function() {
 		// Smooth Scroll
 		document.querySelector('.about').scrollIntoView({ behavior: 'smooth' });
+		hideMobileMenu();	
 	});
 	
 	//-----------------------
@@ -12,6 +13,7 @@ $(document).ready(function() {
 	$('li.markt-menu a').on('click', function() {
 		// Smooth Scroll
 		document.querySelector('.marktplaats').scrollIntoView({ behavior: 'smooth' });
+		hideMobileMenu();	
 	});
 	
 	//-----------------------
@@ -19,6 +21,7 @@ $(document).ready(function() {
 	$('li.fotos-menu a').on('click', function() {
 		// Smooth Scroll
 		document.querySelector('.fotos').scrollIntoView({ behavior: 'smooth' });
+		hideMobileMenu();	
 	});
 	
 	//-----------------------
@@ -26,9 +29,35 @@ $(document).ready(function() {
 	$('li.logo-menu a').on('click', function() {
 		// Smooth Scroll
 		document.querySelector('.home-anchor').scrollIntoView({ behavior: 'smooth' });
+		hideMobileMenu();	
 	});
 	
 	
+	
+	// Show Mobile Menu
+	$('.mobile-menu-button-open').on('click', function() {
+		showMobileMenu();
+	});
+	// Hide Mobile Menu
+	$('.mobile-menu-button-closed').on('click', function() {
+		hideMobileMenu();		
+	});
+	
+	function showMobileMenu() {
+		// hide menu
+		$(".header .menu-wrapper").css({'display': 'block', 'height': 'auto'});				
+		// hide bars, show times
+		$(this).css({'display': 'none'});
+		$('.mobile-menu-button-closed').css({'display': 'block'});
+	}
+	
+	function hideMobileMenu() {
+		// hide menu
+		$(".header .menu-wrapper").css({'display': 'none', 'height': '0'});		
+		// hide times, show bars
+		$(this).css({'display': 'none'});
+		$('.mobile-menu-button-open').css({'display': 'block'});
+	}
 })
 
 
